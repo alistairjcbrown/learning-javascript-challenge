9bf4bf6978b74359b95b7e7ebfc4498c10a3bc07
Alistair Brown <github@alistairjcbrown.com>
2014-08-09 00:21:08 +0100

    Before starting

--- js/app.js
+++ js/app.js
@@ -1,17 +1,57 @@
 $(document).ready(function() {
 
     // Javascript code goes in here
-    var list_of_characters = [];
-
-    $("li > img").each(function() {
-        list_of_characters.push({
-            source: $(this).attr("src"),
-            name:   $(this).attr("data-name"),
-            age:    $(this).attr("data-age"),
-            gender: $(this).attr("data-gender"),
-            parent_class: $(this).parent().attr("class")
-        });
-    });
+    var list_of_characters = [
+        {
+            "source": "http://www.cartoonnetwork.co.uk/sites/www.cartoonnetwork.co.uk/files/imagecache/tv_show_character_thumb/finn_thumb.png",
+            "name": "Finn",
+            "age": "8",
+            "gender": "male",
+            "parent_class": "js-finn-image"
+        },
+        {
+            "source": "http://www.cartoonnetwork.co.uk/sites/www.cartoonnetwork.co.uk/files/imagecache/tv_show_character_thumb/jake_thumb.png",
+            "name": "Jake",
+            "age": "30",
+            "gender": "male",
+            "parent_class": "js-jake-image"
+        },
+        {
+            "source": "http://www.cartoonnetwork.co.uk/sites/www.cartoonnetwork.co.uk/files/imagecache/tv_show_character_thumb/beemo_thumb.png",
+            "name": "Beemo",
+            "age": "2",
+            "gender": "female",
+            "parent_class": "js-beemo-image"
+        },
+        {
+            "source": "http://www.cartoonnetwork.co.uk/sites/www.cartoonnetwork.co.uk/files/imagecache/tv_show_character_thumb/ice_king_thumb.png",
+            "name": "Ice King",
+            "age": "40",
+            "gender": "male",
+            "parent_class": "js-ice-king-image"
+        },
+        {
+            "source": "http://www.cartoonnetwork.co.uk/sites/www.cartoonnetwork.co.uk/files/imagecache/tv_show_character_thumb/gunther_thumb.png",
+            "name": "Gunther",
+            "age": "4",
+            "gender": "male",
+            "parent_class": "js-gunther-image"
+        },
+        {
+            "source": "http://www.cartoonnetwork.co.uk/sites/www.cartoonnetwork.co.uk/files/imagecache/tv_show_character_thumb/princess_bubblegum_thumb.png",
+            "name": "Princess Bubblegum",
+            "age": "18",
+            "gender": "female",
+            "parent_class": "js-princess-bubblegum-image"
+        },
+        {
+            "source": "http://www.cartoonnetwork.co.uk/sites/www.cartoonnetwork.co.uk/files/imagecache/tv_show_character_thumb/lemongrab_thumb.png",
+            "name": "Lemon Grab",
+            "age": "30",
+            "gender": "male",
+            "parent_class": "js-lemon-grab-image"
+        }
+    ];
 
     console.log(list_of_characters);
 
