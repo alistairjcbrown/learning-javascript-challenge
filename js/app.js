1833ec57677a3a1b41aa387c5b6076e4638145bf
Alistair Brown <github@alistairjcbrown.com>
2014-08-09 00:08:10 +0100

    Task complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
     // Javascript code goes in here
-    console.log("test");
+    var list_of_characters = [];
+
+    $("li > img").each(function() {
+        list_of_characters.push({
+            source: $(this).attr("src"),
+            name:   $(this).attr("data-name"),
+            age:    $(this).attr("data-age"),
+            gender: $(this).attr("data-gender"),
+            parent_class: $(this).parent().attr("class")
+        });
+    });
+
+    console.log(list_of_characters);
 
 });
